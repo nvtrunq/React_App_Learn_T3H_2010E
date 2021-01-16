9f0a018a8d8090e0b737b4e9fc87b9dbaa60e993
trungnguyen0111 <nvtrungict@gmail.com>
2021-01-16 17:02:32 +0700

    add Homework/unit19

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import './scss/index.scss'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Users from './components/Users'
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Users />
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
